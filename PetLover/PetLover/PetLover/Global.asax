16220765ffb0385f4a20dd58622e06382d40170a
pablodouglass <39567310+pablodouglass@users.noreply.github.com>
2025-01-28 18:33:39 -0600

    Archivo Base del proyecto
    Aqui esta practica 1 ya realizado

---
+++ PetLover/PetLover/PetLover/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PetLover.MvcApplication" Language="C#" %>
---
+++ PetLover/PetLover/PetLover/Models/PersonasModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetLover.Models
+{
+    public class PersonasModel
+    {
+        public string Identificacion { get; set; }
+        public string Nombre { get; set; }
+
+    }
+}
